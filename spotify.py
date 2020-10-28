039847236542463e0e7d73b087eb0f87123e94ef
Charles Charlestin <ccharlestin@gmail.com>
2020-10-27 21:34:19 -0400

    Optimized functions and fixed bugs

--- spotify.py
+++ spotify.py
@@ -19,7 +19,7 @@ def printAllLikedSongs(spotipyObj):
     limit = 50 # Max number of songs that can be in dictionary (result) at once 0-49
     count = 0
     results = spotipyObj.current_user_saved_tracks(limit, offset)
-    while offset != 10000: # 10000 is max number of songs a playlist can contain (Not efficient, find better way to end while loop)
+    while offset <= results['total']:
         for idx, item in enumerate(results['items']):
             track = item['track']
             print(count, track['artists'][0]['name'], " – ", track['name'])
@@ -49,22 +49,6 @@ def viewPlaylistInfo(spotipyObj):
     except:
         print('Invalid Playlist ID')
 
-
-def addSongToPlaylist(spotipyObj):
-    playlist_id = input("Please input the playlist ID (Copy Spotify URI): ")
-    track_id = input("Please input the song ID (Copy Spotify URI): ")
-   
-    try:
-        if 'spotify:track:' in track_id: 
-            track_id.replace('spotify:track:','')
-        if 'spotify:playlist:' in playlist_id: #Removes the spotify:track prefix whe coppying the Spotify URI
-            playlist_id.replace('spotify:playlist:','')  
-        arr = [track_id]
-        spotipyObj.playlist_add_items(playlist_id, arr)
-        print('Song added to playlist')
-    except:
-        print('Invalid Playlist or Track ID')
-
 def getSongURI(headers):
     title = input('Input title: ')
     artist = input('Input artist: ')
@@ -74,18 +58,26 @@ def getSongURI(headers):
     print(r.json()['tracks']['items'][0]['uri'])
 
 
-def createPlaylist(username):
-    name = input('What is name of the new playlist: ')
-    spotipyUser.user_playlist_create(username, name)
+def createPlaylist(spotipyObj):
+    playlist_name = input('What is name of the new playlist: ')
+    userInfo = spotipyObj.me()
+    caller_name =  userInfo['display_name']
+    spotipyObj.user_playlist_create(caller_name, playlist_name)
 
-def addToPlaylist(headers):
+def addToPlaylist(headers, spotipyObj):
     playlist_name = input('What is the name of the playlist you are adding to: ')
-    
     BASE_URL = 'https://api.spotify.com/v1/'
     r = requests.get(BASE_URL + 'search?q={}&type=playlist&limit=1'.format(playlist_name), headers=headers)
-    print(r.json())
-    #tracks = ['spotify:track:4wQFB68968Q08qP3iy5DMW', 'spotify:track:6IwKcFdiRQZOWeYNhUiWIv', 'spotify:track:07G9Dbpg14PlUFstgf32id', 'spotify:track:07G9Dbpg14PlUFstgf32id']
-    #spotipyUser.user_playlist_add_tracks('ccharlestin', 'spotify:playlist:2aJDqNNgTqiEUlTjq8R9Mj', tracks)
+    playlist_owner = r.json()['playlists']['items'][0]['owner']['display_name']
+    userInfo = spotipyObj.me()
+    caller_name =  userInfo['display_name']
+    if playlist_owner == caller_name:
+        print('You own this playlist, but I didn\'t create the functionality yet')
+        #tracks = ['spotify:track:4wQFB68968Q08qP3iy5DMW', 'spotify:track:6IwKcFdiRQZOWeYNhUiWIv', 'spotify:track:07G9Dbpg14PlUFstgf32id', 'spotify:track:07G9Dbpg14PlUFstgf32id']
+        #spotipyUser.user_playlist_add_tracks('ccharlestin', 'spotify:playlist:2aJDqNNgTqiEUlTjq8R9Mj', tracks)
+    else:
+        print('You can only motify playlists that you own')
+    
 
 
 def userAuthentication():
@@ -126,20 +118,19 @@ def menu():
       print('What do you want to do')
       print('0. Quit Program')
       print('1. View user information')
-      print('2. View like songs') # Need to find way to get user total like songs w/out iterating 10k times 
+      print('2. View like songs')
       print('3. View track information')
       print('4. View playlist information')
-      print('5. Add a single song to playlist') # Modify and/or remove function
-      print('6. Get Song URI')
-      print('7. Create Playlist')
-      print('8. Add song(s) to playlist') #Needs work (Restrict adding to playlist that doesn't match user_id)
+      print('5. Get Song URI')
+      print('6. Create Playlist')
+      print('7. Add song(s) to playlist') #Needs work (Functionality needed)
       
 
 # https://stmorse.github.io/journal/spotify-api.html (For access token help)
 # https://github.com/rach-sharp/spotipy/commit/b051e2a164815dd5c966382d5f1b0ce05fafd36a (Use later)
 def main():
     spotipyUser = userAuthentication()
-
+ 
     while True:
         menu()
         choice = input('Input here: ')
@@ -152,13 +143,11 @@ def main():
         elif choice == '4':
             viewPlaylistInfo(spotipyUser)
         elif choice == '5':
-            addSongToPlaylist(spotipyUser)
-        elif choice == '6':
             getSongURI(spotipyUser._auth_headers())
+        elif choice == '6':
+            createPlaylist(spotipyUser)
         elif choice == '7':
-            createPlaylist(username)
-        elif choice == '8':
-            addToPlaylist(spotipyUser._auth_headers())     
+            addToPlaylist(spotipyUser._auth_headers(), spotipyUser)     
         elif choice == '0':
             exit()
 
